e3eb1a4248fe1a135329a32da88c5037198a6816
rrsilaya <rrsilaya@up.edu.ph>
2017-12-15 14:21:04 +0800

    Execute starter CLI commands
    - Initialized git repository
    - Install project dependencies

--- index.js
+++ index.js
@@ -2,9 +2,19 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { exec } = require('child_process');
+const { Spinner } = require('cli-spinner');
+
+const C = {
+  blue: '\x1b[34m',
+  reset: '\x1b[0m',
+  green: '\x1b[32m'
+}
 
 const TEMPLATES = fs.readdirSync(`${__dirname}/templates`);
 const PWD = process.cwd();
+const LOADER = new Spinner(`${C.blue}%s${C.reset} Installing dependencies (this may take a while)...`);
+LOADER.setSpinnerString("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏");
 
 const validateProject = input => {
   if (fs.existsSync(`${PWD}/${input}`)) return 'Project already exists.'
@@ -32,6 +42,8 @@ const populateFiles = (boilerplate, project) => {
       const writePath = `${PWD}/${project}/${file}`;
       fs.writeFileSync(writePath, contents, 'utf8');
     } else if (stats.isDirectory()) {
+      if (file === 'node_modules') return;
+
       fs.mkdirSync(`${PWD}/${project}/${file}`);
       populateFiles(`${boilerplate}/${file}`, `${project}/${file}`);
     }
@@ -48,15 +60,39 @@ const QUESTIONS = [
   {
     name: 'project',
     type: 'input',
-    message: 'Project Name:',
+    message: 'Project name:',
     validate: validateProject
   }
 ];
+
+process.stdout.write(`${C.blue}
+                                         __
+                       ____ _____  _____/ /______
+                      / __ \`/ __ \\/ ___/ __/ ___/
+                     / /_/ / / / / /  / /_/ /
+                     \\__, /_/ /_/_/   \\__/_/
+                    /____/
+            ${C.green}Your starter files will be generated shortly.
+${C.reset}\n`);
 
 inquirer.prompt(QUESTIONS).then(answers => {
   const { boilerplate, project } = answers;
   const path = `${__dirname}/templates/${boilerplate}`;
 
   fs.mkdirSync(`${PWD}/${project}`);
+  process.stdout.write('\n');
   populateFiles(path, project);
+  process.stdout.write(`${C.green}✓${C.reset} Generated ${C.blue}${boilerplate}${C.reset} starter files\n`);
+
+  LOADER.start();
+  exec(`cd ${project} && git init && yarn`, err => {
+    if (err) {
+      process.stdout.write('Unknown error occured\n');
+      return;
+    }
+
+    LOADER.stop(true);
+    process.stdout.write(`${C.green}✓${C.reset} Installed dependencies\n\n`);
+    process.stdout.write(`${C.blue}You're good to go! Happy hacking!${C.reset}\n`);
+  });
 });
